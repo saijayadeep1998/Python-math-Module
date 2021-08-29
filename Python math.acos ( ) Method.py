f8e01dcfb8b26ec769bc1c81cef76197bf79a343
saijayadeep1998 <79976162+saijayadeep1998@users.noreply.github.com>
2021-08-29 21:19:29 +0530

    Create Python math.acos ( ) Method.py

---
+++ Python math.acos ( ) Method.py
@@ -0,0 +1,8 @@
+# Import math Library
+import math
+# Return the arc cosine of numbers
+print(math.acos(0.55))
+print(math.acos(-0.55))
+print(math.acos(0))
+print(math.acos(1))
+print(math.acos(-1))
