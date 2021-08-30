53b2101171275e9a17af12ad94731a259aeed011
saijayadeep1998 <79976162+saijayadeep1998@users.noreply.github.com>
2021-08-30 20:33:51 +0530

    Create math.asinh( ) Method.py

---
+++ math.asinh( ) Method.py
@@ -0,0 +1,9 @@
+# Import math Library
+import math
+# Return the hyperbolic arc sine value of numbers
+print(math.asinh(7))
+print(math.asinh(56))
+print(math.asinh(2.45))
+print(math.asinh(1))
+print(math.asinh(0.5))
+print(math.asinh(-10))
