8cf3bafa186e96ffe51a5d8daa9f93022ec18249
saijayadeep1998 <79976162+saijayadeep1998@users.noreply.github.com>
2021-08-30 20:31:11 +0530

    Create math.acosh( ) Method.py

---
+++ math.acosh( ) Method.py
@@ -0,0 +1,7 @@
+# Import math Library
+import math
+# Return the inverse hyperbolic cosine of different numbers
+print(math.acosh(7))
+print(math.acosh(56))
+print(math.acosh(2.45))
+print(math.acosh(1))
