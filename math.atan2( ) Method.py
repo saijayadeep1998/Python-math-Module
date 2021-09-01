98c85298caf2a740c992e7fcd4673f7009c2f1da
saijayadeep1998 <79976162+saijayadeep1998@users.noreply.github.com>
2021-09-01 21:21:58 +0530

    Create math.atan2( ) Method.py

---
+++ math.atan2( ) Method.py
@@ -0,0 +1,7 @@
+# Import math Library
+import math
+# Return the arc tangent of y/x in radians
+print(math.atan2(8, 5))
+print(math.atan2(20, 10))
+print(math.atan2(34, -7))
+print(math.atan2(-340, -120))
