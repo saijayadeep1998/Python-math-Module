4cafeaa530b0e3d6150ab2ed5056ce64243d99c2
saijayadeep1998 <79976162+saijayadeep1998@users.noreply.github.com>
2021-09-02 21:45:31 +0530

    Create math.copysign() Method.py

---
+++ math.copysign() Method.py
@@ -0,0 +1,6 @@
+#Import math Library
+import math  
+#Return the value of the first parameter and the sign of the second parameter
+print(math.copysign(4, -1))
+print(math.copysign(-8, 97.21))
+print(math.copysign(-43, -76))
