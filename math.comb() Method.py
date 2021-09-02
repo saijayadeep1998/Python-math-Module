f59413bf9bba2309ae75225ac47b7897b0902050
saijayadeep1998 <79976162+saijayadeep1998@users.noreply.github.com>
2021-09-02 21:44:21 +0530

    Create math.comb() Method.py

---
+++ math.comb() Method.py
@@ -0,0 +1,8 @@
+# Import math Library
+import math
+# Initialize the number of items to choose from
+n = 7
+# Initialize the number of possibilities to choose
+k = 5
+# Print total number of possible combinations
+print (math.comb(n, k))
