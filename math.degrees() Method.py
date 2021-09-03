6f6bbc81eae138d7a2dd715ccae27078d469e7b4
saijayadeep1998 <79976162+saijayadeep1998@users.noreply.github.com>
2021-09-03 20:00:59 +0530

    Create math.degrees() Method.py

---
+++ math.degrees() Method.py
@@ -0,0 +1,7 @@
+#Import math Library
+import math 
+#Convert angles from radians to degrees:
+print (math.degrees(8.90))
+print (math.degrees(-20))
+print (math.degrees(1))
+print (math.degrees(90))
