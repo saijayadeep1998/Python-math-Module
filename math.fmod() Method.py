231fce9d65bd0861005c62e63ca591b930f190d4
saijayadeep1998 <79976162+saijayadeep1998@users.noreply.github.com>
2021-09-06 21:29:40 +0530

    Create math.fmod() Method.py

---
+++ math.fmod() Method.py
@@ -0,0 +1,8 @@
+# Import math Library
+import math 
+# Return the remainder of x/y
+print(math.fmod(20, 4))
+print(math.fmod(20, 3))
+print(math.fmod(15, 6))
+print(math.fmod(-10, 3))
+print(math.fmod(0, 0))
