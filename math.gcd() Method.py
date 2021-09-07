4416edd64051a8b8ae4acc36236e519b13149dfa
saijayadeep1998 <79976162+saijayadeep1998@users.noreply.github.com>
2021-09-07 19:56:03 +0530

    Create math.gcd() Method.py

---
+++ math.gcd() Method.py
@@ -0,0 +1,11 @@
+#Import math Library
+import math
+#find the  the greatest common divisor of the two integers
+print (math.gcd(3, 6))
+print (math.gcd(6, 12))
+print (math.gcd(12, 36))
+print (math.gcd(-12, -36))
+print (math.gcd(5, 12))
+print (math.gcd(10, 0))
+print (math.gcd(0, 34))
+print (math.gcd(0, 0))
