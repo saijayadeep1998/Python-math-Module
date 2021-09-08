8c2cd9613573914614fe654ca908758663072619
saijayadeep1998 <79976162+saijayadeep1998@users.noreply.github.com>
2021-09-08 20:15:59 +0530

    Create math.isclose() Method.py

---
+++ math.isclose() Method.py
@@ -0,0 +1,7 @@
+#Import math Library
+import math  
+#compare the closeness of two values
+print(math.isclose(1.233, 1.4566))
+print(math.isclose(1.233, 1.233))
+print(math.isclose(1.233, 1.24))
+print(math.isclose(1.233, 1.233000001))
