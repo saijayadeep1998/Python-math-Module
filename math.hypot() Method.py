1cd11f4d7c7ecdfe5bbc4e3f758b8da62e45d316
saijayadeep1998 <79976162+saijayadeep1998@users.noreply.github.com>
2021-09-08 20:15:25 +0530

    Create math.hypot() Method.py

---
+++ math.hypot() Method.py
@@ -0,0 +1,7 @@
+# Import math Library
+import math  
+#set perpendicular and base
+parendicular = 10
+base = 5
+#print the hypotenuse of a right-angled triangle
+print(math.hypot(parendicular, base))
