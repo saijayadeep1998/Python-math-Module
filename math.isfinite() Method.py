c8e0b4cc64fb85100b083ef5170ec6fabec85059
saijayadeep1998 <79976162+saijayadeep1998@users.noreply.github.com>
2021-09-08 20:16:46 +0530

    Create math.isfinite() Method.py

---
+++ math.isfinite() Method.py
@@ -0,0 +1,12 @@
+# Import math Library
+import math
+# Check whether the values are finite or not
+print(math.isfinite(2000))
+print(math.isfinite(-45.34))
+print(math.isfinite(+45.34))
+print(math.isfinite(math.inf))
+print(math.isfinite(float("nan")))
+print(math.isfinite(float("inf")))
+print(math.isfinite(float("-inf")))
+print(math.isfinite(-math.inf))
+print(math.isfinite(0.0))
