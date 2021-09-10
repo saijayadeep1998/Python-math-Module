a83768cf0372534616ffa9fbefee5ffa1890b5aa
saijayadeep1998 <79976162+saijayadeep1998@users.noreply.github.com>
2021-09-10 19:28:04 +0530

    Create math.isnan() Method.py

---
+++ math.isnan() Method.py
@@ -0,0 +1,11 @@
+# Import math Library
+import math
+# Check whether some values are NaN
+print (math.isnan (56))
+print (math.isnan (-45.34))
+print (math.isnan (+45.34))
+print (math.isnan (math.inf))
+print (math.isnan (float("nan")))
+print (math.isnan (float("inf")))
+print (math.isnan (float("-inf")))
+print (math.isnan (math.nan))
