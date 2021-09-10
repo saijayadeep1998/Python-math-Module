9eeabee89cc1ed16761b364a5b01d26cb8c36442
saijayadeep1998 <79976162+saijayadeep1998@users.noreply.github.com>
2021-09-10 19:28:50 +0530

    Create math.isqrt() Method.py

---
+++ math.isqrt() Method.py
@@ -0,0 +1,12 @@
+# Import math Library
+import math
+# Examples to print the square root of different numbers
+print (math.sqrt(10))
+print (math.sqrt (12))
+print (math.sqrt (68))
+print (math.sqrt (100))
+# Round square root numbers downward to the nearest integer
+print (math.isqrt(10))
+print (math.isqrt (12))
+print (math.isqrt (68))
+print (math.isqrt (100))
