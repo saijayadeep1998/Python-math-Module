f239486d185c64da670353003ae527f07e10faef
saijayadeep1998 <79976162+saijayadeep1998@users.noreply.github.com>
2021-09-10 19:27:15 +0530

    Create math.isinf() Method.py

---
+++ math.isinf() Method.py
@@ -0,0 +1,11 @@
+# Import math Library
+import math
+# Check whether some values are infinite
+print (math.isinf (56))
+print (math.isinf (-45.34))
+print (math.isinf (+45.34))
+print (math.isinf (math.inf))
+print (math.isinf (float("nan")))
+print (math.isinf (float("inf")))
+print (math.isinf (float("-inf")))
+print (math.isinf (-math.inf))
