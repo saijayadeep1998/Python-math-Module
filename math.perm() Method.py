203446dd7c067b5ba44d8e1858c104b672af5b98
saijayadeep1998 <79976162+saijayadeep1998@users.noreply.github.com>
2021-09-14 21:46:32 +0530

    Create math.perm() Method.py

---
+++ math.perm() Method.py
@@ -0,0 +1,8 @@
+# Import math Library
+import math
+# Initialize the number of items to choose from
+n = 7
+# Initialize the number of items to choose
+k = 5
+# Print the number of ways to choose k items from n items
+print (math.perm(n, k))
