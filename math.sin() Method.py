f24ac7c999e548c18303ad58e14857961eaece6d
saijayadeep1998 <79976162+saijayadeep1998@users.noreply.github.com>
2021-09-15 20:20:45 +0530

    Create math.sin() Method.py

---
+++ math.sin() Method.py
@@ -0,0 +1,8 @@
+# Import math Library
+import math
+# Return the sine of different numbers
+print (math.sin(0.00))
+print (math.sin(-1.23))
+print (math.sin(10))
+print (math.sin(math.pi))
+print (math.sin(math.pi/2))
