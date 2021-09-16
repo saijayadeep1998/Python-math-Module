51e5e19f1d0fe9dfbd5819def3d724a32ace877f
saijayadeep1998 <79976162+saijayadeep1998@users.noreply.github.com>
2021-09-16 22:14:09 +0530

    Create math.sinh() Method.py

---
+++ math.sinh() Method.py
@@ -0,0 +1,8 @@
+# Import math Library
+import math  
+# Return the hyperbolic sine of different values
+print(math.sinh(0.00))
+print(math.sinh(-23.45))
+print(math.sinh(23))
+print(math.sinh(1.00))
+print(math.sinh(math.pi))
